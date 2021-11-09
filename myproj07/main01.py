98d64dc4122b1e65faedf8b5b7c186d47c0d0e4f
JunSoo Lee <ljsay1113@naver.com>
2021-11-09 10:40:31 +0900

    좋아요 수로 TOP10 곡명을 출력

---
+++ myproj07/main01.py
@@ -0,0 +1,16 @@
+from typing import List
+import pandas as pd
+
+df = pd.read_csv("https://bit.ly/3nsLDXy")
+song_list = list(df.T.to_dict().values())
+
+# 좋아요 수로 TOP10 곡명 리스트
+def pick_like_value(song_dict):
+    return song_dict["like"]
+
+
+sorted_song_list = sorted(song_list, key=pick_like_value, reverse=True)
+top10_song_list = sorted_song_list[:10]
+
+for song_dict in top10_song_list:
+    print("{like} {title}".format(**song_dict))
