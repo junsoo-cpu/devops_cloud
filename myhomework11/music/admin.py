1d43c9558376501b3cac21cb550815716707f270
JunSoo Lee <ljsay1113@naver.com>
2021-11-30 18:49:19 +0900

    admin 등록 및 데이터 추가

--- myhomework11/music/admin.py
+++ myhomework11/music/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from music.models import List
 
-# Register your models here.
+class ListAdmin(admin.ModelAdmin):
+    list_display = ["name", "singer", "created_at"]
+    search_fields = ["name"]
+
+admin.site.register(List, ListAdmin)
+
