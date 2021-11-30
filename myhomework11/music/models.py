cdb9d4d85ce974affd2d653fa85850bf022ac69c
JunSoo Lee <ljsay1113@naver.com>
2021-11-30 18:24:19 +0900

    모델 생성 및 마이그레이션

--- myhomework11/music/models.py
+++ myhomework11/music/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class List(models.Model):
+    name = models.CharField(max_length=100, verbose_name="제목")
+    singer = models.CharField(max_length=100, verbose_name="가수명")
+    created_at = models.DateTimeField(auto_now_add=True)
+    content = models.TextField(verbose_name="한줄 평")  # FK키를 쓰는 게 적절
+    photo = models.ImageField(blank=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+    # upload_to
+    # - 문자열 : 파일이 저장되는 폴더의 경로
+
+
