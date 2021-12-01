c96ab9d40cf893078f94a7d6c7792fb6461581f8
JunSoo Lee <ljsay1113@naver.com>
2021-12-01 19:23:23 +0900

    개발모드에서 SQL 화면 출력 로깅 활성화

--- myhomework13/myhomework13/settings.py
+++ myhomework13/myhomework13/settings.py
@@ -136,3 +136,23 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+INTERNAL_IPS = ['127.0.0.1']
+if DEBUG :
+    LOGGING = {         # 로그 표현, 개발 시 용이, 개발 모드에서
+        "version": 1,
+        "disable_existing_loggers": False,
+        "handlers": {
+            "console": {
+                "level": "DEBUG",
+                "class": "logging.StreamHandler",   # 화면에 출력
+            },
+        },
+        "loggers": {
+            "django.db.backends": {
+                "handlers": ["console"],
+                "level": "DEBUG",
+                "propagate": False,
+            },
+        },
+    }
