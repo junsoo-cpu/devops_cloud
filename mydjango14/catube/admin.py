f1a7e4751d33e742d135675cca7267c53747451a
JunSoo Lee <ljsay1113@naver.com>
2021-12-02 14:54:09 +0900

    Catube 구현

--- mydjango14/catube/admin.py
+++ mydjango14/catube/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from catube.models import Video
+
+class VideoAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Video, VideoAdmin)
--- mydjango14/catube/views.py
+++ mydjango14/catube/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
+from catube.models import Video
 
-def index(request :HttpRequest) -> HttpResponse:
-    return render(request, "catube/index.html")
+
+def index(request: HttpRequest) -> HttpResponse:
+    qs = Video.objects.all()  # qs -> lazy(게으르다) 값이 필요한 시점에 찾아와서
+    return render(request, "catube/index.html", {
+        "video_list": qs,
+    })
