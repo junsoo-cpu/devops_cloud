acfd0fdeccb758c27e74f736dcf120b8ce2f054a
JunSoo Lee <ljsay1113@naver.com>
2021-12-02 10:43:16 +0900

    mydjango 세팅 변경

--- mydjango13/mydjango13/settings.py
+++ mydjango13/mydjango13/settings.py
@@ -133,7 +133,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = BASE_DIR / 'media'     # 프로그램 세팅 정도
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
