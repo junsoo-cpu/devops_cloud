f242dbceb460c24d664deae0eda3f6b45b044f32
JunSoo Lee <ljsay1113@naver.com>
2021-12-02 19:23:23 +0900

    django-imagekit 라이브러리를 활용

--- myhomework14/music/models.py
+++ myhomework14/music/models.py
@@ -1,10 +1,17 @@
 from django.db import models
 from imagekit.models import ImageSpecField
+from imagekit.processors import ResizeToFill
 
 class Video(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
     video_file = models.FileField()
     thumbnail_file = models.ImageField()
+    thumbnail_file_thumb = ImageSpecField(
+        source="thumbnail_file",
+        processors=[ResizeToFill(800, 400)],
+        format="JPEG",
+        options={"quality": 60},
+    )
     create_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
