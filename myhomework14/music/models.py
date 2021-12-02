330ec0ff62d17d501cfe8db34e40f65bf89acaf5
JunSoo Lee <ljsay1113@naver.com>
2021-12-02 18:32:04 +0900

    bootstrap 5 cdn 적용

--- myhomework14/music/models.py
+++ myhomework14/music/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from imagekit.models import ImageSpecField
 
 class Video(models.Model):
     title = models.CharField(max_length=200)
--- myhomework14/music/views.py
+++ myhomework14/music/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
 
-# Create your views here.
+
+from music.models import Video
+
+
+def index(request: HttpRequest) -> HttpResponse:
+    qs = Video.objects.all()  # qs -> lazy(게으르다) 값이 필요한 시점에 찾아와서
+    return render(request, "music/index.html", {
+        "video_list": qs,
+    })
