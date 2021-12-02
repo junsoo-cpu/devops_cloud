98edd698e65d3d809c3ede2f96814334bb28455d
JunSoo Lee <ljsay1113@naver.com>
2021-12-02 15:46:05 +0900

    CBV 예시

--- mydjango14/catube/views.py
+++ mydjango14/catube/views.py
@@ -1,13 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
+from django.views.generic import ListView
+
 from catube.models import Video
 
+# class Based View (CBV)
+index = ListView.as_view(model=Video,
+                         template_name="catube/index.html")
 
-def index(request: HttpRequest) -> HttpResponse:
-    qs = Video.objects.all()  # qs -> lazy(게으르다) 값이 필요한 시점에 찾아와서
-    return render(request, "catube/index.html", {
-        "video_list": qs,
-    })
+# Function Based View (FBV)
+# def index(request: HttpRequest) -> HttpResponse:
+#     qs = Video.objects.all()  # qs -> lazy(게으르다) 값이 필요한 시점에 찾아와서
+#     return render(request, "catube/index.html", {
+#         "video_list": qs,
+#     })
 
 
 def video_detail(request: HttpRequest, pk: int) -> HttpResponse:
