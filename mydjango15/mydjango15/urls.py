f5fb73aebb461a82b8d4611ba3f5485bd3188d5a
JunSoo Lee <ljsay1113@naver.com>
2021-12-06 15:48:30 +0900

    namespace, url reverse

--- mydjango15/mydjango15/urls.py
+++ mydjango15/mydjango15/urls.py
@@ -1,12 +1,9 @@
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from delicious.views import shop_list, shop_detail, shop_new_1, shop_new
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('delicious/', shop_list),
-    path('delicious/<int:pk>/', shop_detail),  # <int:pk>정수 패턴이 오면 pk 로 받아준다
-    path('delicious/new1/', shop_new_1),
-    path('delicious/new/', shop_new),
+    path('delicious/', include('delicious.urls')),
 ]
