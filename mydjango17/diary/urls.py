cbe62f9762c31db710cf8eafbeeeb857c3bb0884
JunSoo Lee <ljsay1113@naver.com>
2021-12-07 16:26:49 +0900

    tag_detail  설정

--- mydjango17/diary/urls.py
+++ mydjango17/diary/urls.py
@@ -15,4 +15,5 @@ urlpatterns = [
     path("", views.post_list, name="post_list"),
     path("<int:pk>/", views.post_detail, name="post_detail"),
     path('', root, name="root"),
+path("tags/<str:tag_name>/", views.tag_detail, name="tag_detail"),
 ]
--- mydjango17/diary/views.py
+++ mydjango17/diary/views.py
@@ -1,19 +1,27 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
-
 from diary.models import Post
 
 
-def post_list(request: HttpRequest) -> HttpResponse:  # 첫번째 인자는 request
+def tag_detail(request: HttpRequest, tag_name: str) -> HttpResponse:
+    qs = Post.objects.all()
+    qs = qs.filter(tag_set__name=tag_name)
+
+    return render(request, "diary/tag_detail.html", {
+        "tag_name": tag_name,
+        "post_list": qs,
+    })
+
+
+def post_list(request: HttpRequest) -> HttpResponse:
     qs = Post.objects.all()
 
     query = request.GET.get("query", "")
     if query:
         qs = qs.filter(title__icontains=query)
-
-    return render(request, "diary/post_list.html", {"post_list": qs})
-
-
+    return render(request, "diary/post_list.html", {
+        "post_list": qs,
+    })
 def post_detail(request: HttpRequest, pk: int) -> HttpResponse:
     post = Post.objects.get(pk=pk)
     comment_list = post.comment_set.all()
