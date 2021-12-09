b45c0f2ad01b58552b9e4b466e1d986123aff20d
JunSoo Lee <ljsay1113@naver.com>
2021-12-09 13:58:10 +0900

    포스팅 쓰기 Form, edit Form 추가

--- mydjango19/diary/urls.py
+++ mydjango19/diary/urls.py
@@ -14,6 +14,7 @@ def root(request):
 urlpatterns = [
     path("", views.post_list, name="post_list"),
     path("<int:pk>/", views.post_detail, name="post_detail"),
-    path('', root, name="root"),
-path("tags/<str:tag_name>/", views.tag_detail, name="tag_detail"),
+    path("new/", views.post_new, name="post_new"),
+    path("<int:pk>/edit/", views.post_edit, name="post_edit"),
+    path("tags/<str:tag_name>/", views.tag_detail, name="tag_detail"),
 ]
--- mydjango19/diary/views.py
+++ mydjango19/diary/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpRequest, HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from diary.forms import PostForm
 from diary.models import Post
 
 
@@ -22,6 +24,8 @@ def post_list(request: HttpRequest) -> HttpResponse:
     return render(request, "diary/post_list.html", {
         "post_list": qs,
     })
+
+
 def post_detail(request: HttpRequest, pk: int) -> HttpResponse:
     post = Post.objects.get(pk=pk)
     comment_list = post.comment_set.all()
@@ -30,4 +34,43 @@ def post_detail(request: HttpRequest, pk: int) -> HttpResponse:
         "post": post,
         "comment_list": comment_list,
         "tag_list": tag_list,
+    })
+
+
+def post_new(request: HttpRequest) -> HttpResponse:
+    # print("request.method :", request.method)
+    # print("request.GET :", request.GET)
+    # print("request.POST :", request.POST)
+    # print("request.FILES :", request.FILES)
+
+    # 서식 입력 값을 전달받아서, 유효성 검사
+    # -> 에러상황에서는 에러메세지를 보여주겠다.
+    # -> 유효성 검사에 통과하면, 입력값을 보여주고, post_list로 이동
+    if request.method == "POST":
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()  # ModelForm에서만 지원
+            return redirect("diary:post_list")
+    else:
+        form = PostForm()
+
+    return render(request, "diary/post_form.html", {
+        "form": form,
+    })
+
+
+def post_edit(request: HttpRequest, pk: int) -> HttpResponse:
+    # 아래 코드는 ModelForm에 한해서 동작하는 코드
+    post = Post.objects.get(pk=pk)
+
+    if request.method == "POST":
+        form = PostForm(request.POST, request.FILES, instance=post)
+        if form.is_valid():
+            form.save()  # ModelForm에서만 지원
+            return redirect("diary:post_list")
+    else:
+        form = PostForm(instance=post)
+
+    return render(request, "diary/post_form.html", {
+        "form": form,
     })
