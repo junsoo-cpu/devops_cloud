f1f26c4c990db1fe1a40e50223ad355500b0d8a6
JunSoo Lee <ljsay1113@naver.com>
2021-12-09 15:09:12 +0900

    title 세부 모델 설정

--- mydjango19/diary/models.py
+++ mydjango19/diary/models.py
@@ -1,3 +1,4 @@
+from django.core.validators import MinLengthValidator
 from django.db import models
 
 
@@ -11,12 +12,17 @@ class TimestampedModel(models.Model):  # 상속
 
 class Post(TimestampedModel):
     author_name = models.CharField(max_length=20)
-    title = models.CharField(max_length=200, db_index=True)  # db_index가 생성되어 있어야 검색 가능
+    title = models.CharField(
+        max_length=200,
+        db_index=True,
+        validators=[
+            MinLengthValidator(3),
+        ]
+    )  # db_index가 생성되어 있어야 검색 가능
     content = models.TextField()
     photo = models.ImageField(upload_to="diary/post/%Y/%m/%d")  # media 바로 밑에 저장, upload to 를 통해 경로 설정
     tag_set = models.ManyToManyField('Tag')  # 참조가 아니어서 Tag를 문자열로 지정 django app에서 Tag를 찾아줌
     ip = models.GenericIPAddressField()
-
 
     def __str__(self) -> str:
         return self.title
