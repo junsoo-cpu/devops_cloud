0875ffb1b20054d6fb6af1a3acb66dcdb2544e8a
JunSoo Lee <ljsay1113@naver.com>
2021-12-13 16:38:18 +0900

    Tag 내역 저장 및 뷰에 구현된 Tag 저장 로직을 ShopForm 클래스로 이동

--- mydjango20/shop/views.py
+++ mydjango20/shop/views.py
@@ -2,21 +2,18 @@ from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render, redirect, get_object_or_404
 
 from shop.froms import ShopForm
-from shop.models import Shop, Category
+from shop.models import Shop, Category, Tag
 
 
 def shop_list(request: HttpRequest) -> HttpResponse:
     category_qs = Category.objects.all()
-    qs = Shop.objects.all()     # .order_by("-id")
-
+    qs = Shop.objects.all()  # .order_by("-id")
     category_id: str = request.GET.get("category_id", "")
     if category_id:
         qs = qs.filter(category__pk=category_id)
-
     query = request.GET.get("query", "")
     if query:
         qs = qs.filter(name__icontains=query)
-
     return render(request, "shop/shop_list.html", {
         "category_list": category_qs,
         "shop_list": qs,
@@ -24,21 +21,22 @@ def shop_list(request: HttpRequest) -> HttpResponse:
 
 
 # /shop/100/
-
-def shop_detail(request: HttpRequest, pk: int)-> HttpResponse:
+def shop_detail(request: HttpRequest, pk: int) -> HttpResponse:
     shop = get_object_or_404(Shop, pk=pk)
-    return render(request, "shop/shop_detail.html",{
-        "shop":shop,
+    return render(request, "shop/shop_detail.html", {
+        "shop": shop,
     })
 
 
-# /shop/new
+# /shop/new/
 def shop_new(request: HttpRequest) -> HttpResponse:
-    # raise NotImplementedError (" 곧 구현 예정")
+    # raise NotImplementedError("곧 구현 예정")
     if request.method == "POST":
         form = ShopForm(request.POST, request.FILES)
         if form.is_valid():
             saved_post = form.save()
+
+            # shop_detail 뷰를 구현했다면 !!!
             return redirect("shop:shop_detail", saved_post.pk)
     else:
         form = ShopForm()
