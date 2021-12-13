a76c0432736e916819dbf78dd43e483e9719db6d
JunSoo Lee <ljsay1113@naver.com>
2021-12-13 18:07:40 +0900

    shop 앱 생성 및 등록

---
+++ myhomework21.1/shop/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from shop.models import Category, Tag, Shop
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Shop)
+class ShopAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    pass
