0760c3464ea1925641b9c6715887c9107ada1e10
JunSoo Lee <ljsay1113@naver.com>
2021-12-13 23:24:16 +0900

    shop_detail 구현 및 shop_list에 category 표현

--- myhomework21/shop/urls.py
+++ myhomework21/shop/urls.py
@@ -5,4 +5,5 @@ from shop import views
 app_name = 'shop'
 urlpatterns = [
     path("", views.shop_list, name="shop_list"),
+    path("<int:pk>/", views.shop_detail, name="shop_detail")
 ]
--- myhomework21/shop/views.py
+++ myhomework21/shop/views.py
@@ -1,11 +1,29 @@
 from django.http import HttpRequest, HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-from shop.models import Shop
+from shop.models import Shop, Category
 
 
 def shop_list(request: HttpRequest) -> HttpResponse:
     qs = Shop.objects.all()
+    category_qs = Category.objects.all()
+
+    category_id: str = request.GET.get("category_id", "")
+    if category_id:
+        qs = qs.filter(category__pk=category_id)
+
+    query = request.GET.get("query", "")
+    if query:
+        qs = qs.filter(name__icontains=query)
+
     return render(request, "shop/shop_list.html", {
         "shop_list": qs,
+        "category_list":category_qs
+    })
+
+
+def shop_detail(request: HttpRequest, pk: int) -> HttpResponse:
+    shop = get_object_or_404(Shop, pk=pk)
+    return render(request, "shop/shop_detail.html", {
+        "shop": shop,
     })
