7036875b8bc03a65d3ca9913dee601e57f872e52
JunSoo Lee <ljsay1113@naver.com>
2021-12-13 23:56:24 +0900

    forms.py 뷰에서 태그 내역 저장

--- myhomework21/shop/forms.py
+++ myhomework21/shop/forms.py
@@ -1,9 +1,27 @@
 from django import forms
 
-from shop.models import Shop
+from shop.models import Shop, Tag
 
 
 class ShopForm(forms.ModelForm):
+    tags = forms.CharField
+
+    def save(self):
+        saved_post = super().save()
+
+        tag_list =[]
+        tags = self.clened_data.get("tags", "")
+        for word in tags.split(","):
+            tag_name = word.strip()
+            tag, __ = Tag.objects.get_or_create(name=tag_name)
+            tag_list.append(tag)
+
+        saved_post.tag_set.clear()
+        saved_post.tag_set.add(*tag_list)
+
     class Meta:
         model = Shop
-        fields = "__all__"
+        fields = ["category",
+                  "title",
+                  "content",
+                  ]
