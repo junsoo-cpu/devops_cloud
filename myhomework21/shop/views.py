3e4e0f2d51604b7910d3b43a023820b0da079584
JunSoo Lee <ljsay1113@naver.com>
2021-12-13 23:38:47 +0900

    shop_new 생성 및 설정

--- myhomework21/shop/views.py
+++ myhomework21/shop/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpRequest, HttpResponse
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 
+from shop.forms import ShopForm
 from shop.models import Shop, Category
 
 
@@ -26,4 +27,19 @@ def shop_detail(request: HttpRequest, pk: int) -> HttpResponse:
     shop = get_object_or_404(Shop, pk=pk)
     return render(request, "shop/shop_detail.html", {
         "shop": shop,
+    })
+
+
+def shop_new(request: HttpRequest) -> HttpResponse:
+    if request.method == "POST":
+        form = ShopForm(request.POST, request.FILES)
+        if form.is_valid():
+            saved_post = form.save()
+            # shop_detail 뷰를 구현했다면 !!!
+            return redirect("shop:shop_detail", saved_post.pk)
+    else:
+        form = ShopForm()
+
+    return render(request, "shop/shop_form.html", {
+        "form": form,
     })
