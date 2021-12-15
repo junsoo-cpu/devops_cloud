157fd105458ecb4f46e67e5663236ec73cf1bbf9
JunSoo Lee <ljsay1113@naver.com>
2021-12-15 17:18:42 +0900

    회원가입 설정

--- mydjango23/accounts/views.py
+++ mydjango23/accounts/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
 
-# Create your views here.
+
+def login(request):
+    pass
+
+
+# signup = CreateView.as_view(
+#     form_class=UserCreationForm,
+#     success_url=reverse_lazy("accounts:login"),
+#     template_name="accounts/signup_form.html",
+# )
+
+def signup(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect("accounts:login")
+    else:
+        form = UserCreationForm()
+
+    return render(request, "accounts/signup_form.html", {
+        "form": form,
+    })
+
+
+def profile(request):
+    pass
+
+
+def logout(request):
+    pass
