e80d6cb2019c2c8fd48ca690b689731d80df2233
JunSoo Lee <ljsay1113@naver.com>
2021-12-16 19:53:45 +0900

    bootstrap v5 적용 및 shop_list, shop_detail 구현

--- myhomework24/shop/views.py
+++ myhomework24/shop/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
 
-# Create your views here.
+from django.views.generic import (
+    ListView,
+    DetailView,
+
+)
+
+
+
+from shop.models import Shop
+
+shop_list = ListView.as_view(
+    model=Shop,
+)
+
+shop_detail = DetailView.as_view(
+    model=Shop,
+)
