0f16d54206a99ccaf6ce554fc045eb9924c628c6
JunSoo Lee <ljsay1113@naver.com>
2021-12-17 16:40:21 +0900

    review 구현

--- mydjango25/shop/views.py
+++ mydjango25/shop/views.py
@@ -1,6 +1,11 @@
-from django.views.generic import ListView, DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.shortcuts import redirect, get_object_or_404, resolve_url
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
 
-from shop.models import Shop, Category
+from shop.forms import ReviewForm
+from shop.mixins import ReviewUserCheckMixin
+from shop.models import Shop, Category, Review
 
 
 class ShopListView(ListView):
@@ -14,8 +19,42 @@ class ShopListView(ListView):
 
 shop_list = ShopListView.as_view()
 
-
 shop_detail = DetailView.as_view(
     model=Shop,
 
 )
+
+
+class ReviewCreateView(LoginRequiredMixin, CreateView):
+    model = Review
+    form_class = ReviewForm
+
+    # success_url = reverse_lazy("shop:shop_list"),
+
+    def form_valid(self, form):
+        # self.kwargs : URL Captured 값들이 사전으로 저장
+        shop_pk = self.kwargs["shop_pk"]
+        shop = get_object_or_404(Shop, pk=shop_pk)
+
+        review = form.save(commit=False)
+        review.shop = shop
+        review.user = self.request.user
+        review.save()
+        # return redirect("shop:shop_detail", shop.pk)
+        return redirect(shop)
+
+
+review_new = ReviewCreateView.as_view()
+
+
+class ReviewUpdateView(LoginRequiredMixin, ReviewUserCheckMixin, UpdateView):
+    model = Review
+    form_class = ReviewForm
+    success_url = reverse_lazy("shop:shop_list")
+
+    def get_success_url(self):
+        review = self.object
+        return resolve_url(review.shop)
+
+
+review_edit = ReviewUpdateView.as_view()
