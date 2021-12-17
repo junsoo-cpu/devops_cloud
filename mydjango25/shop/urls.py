8da2c8f3d9fdcd28dabe6ccff32f3919da5a3191
JunSoo Lee <ljsay1113@naver.com>
2021-12-17 14:48:30 +0900

    shop_detail 구현

--- mydjango25/shop/urls.py
+++ mydjango25/shop/urls.py
@@ -4,5 +4,6 @@ from shop import views
 app_name="shop"
 
 urlpatterns=[
-    path("", views.shop_list, name="shop_list")
+    path("", views.shop_list, name="shop_list"),
+    path("<int:pk>/", views.shop_detail, name="shop_detail"),
 ]
