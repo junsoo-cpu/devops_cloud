e3a912c0015f7979873e07d8fcee69b3fc45ab60
JunSoo Lee <ljsay1113@naver.com>
2021-12-23 14:12:44 +0900

    MelonTop100 생성 및 css 적용

---
+++ myreact29/src/MelonTop100.jsx
@@ -0,0 +1,32 @@
+import { useState } from 'react';
+import initialSongList from './data/melon_data.json';
+import './MelonTop100.css';
+
+function MelonTop100() {
+  const [songList, setSongList] = useState([]);
+
+  const handleClick = () => {
+    setSongList(initialSongList);
+  };
+
+  return (
+    <div>
+      <h2>멜론 top 100</h2>
+      <button onClick={handleClick}>로딩</button>
+      <ul className="songList">
+        {songList.map((song) => {
+          return (
+            <li>
+              {song.artist} : {song.title}
+            </li>
+          );
+        })}
+        <li>제목 1</li>
+        <li>제목 2</li>
+        <li>제목 3</li>
+      </ul>
+    </div>
+  );
+}
+
+export default MelonTop100;
