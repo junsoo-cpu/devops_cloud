0f2ce34f0ea5527817d491ccdfed24d36f5c7e1e
JunSoo Lee <ljsay1113@naver.com>
2021-12-23 15:23:54 +0900

    api서버에 axios 요청을 통해 song list에 반영

--- myreact29/src/MelonTop100.jsx
+++ myreact29/src/MelonTop100.jsx
@@ -1,24 +1,45 @@
 import { useState } from 'react';
 import { Button as BootstrapButton } from 'react-bootstrap';
 import { Button as AntdButton } from 'antd';
+import Axios from 'axios';
 import initialSongList from './data/melon_data.json';
 import './MelonTop100.css';
 
 function MelonTop100() {
   const [songList, setSongList] = useState([]);
 
-  const handleClick = () => {
+  const handleClick1 = () => {
     setSongList(initialSongList);
+  };
+
+  const handleClick2 = () => {
+    const url = 'https://antigravity-daejeon-2021.herokuapp.com/api/melon/';
+    Axios.get(url)
+      .then((response) => {
+        const { data } = response;
+        setSongList(data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
+  const handleClick3 = () => {
+    setSongList([]);
   };
 
   return (
     <div>
       <h2>멜론 top 100</h2>
-      <BootstrapButton variant="success" onClick={handleClick}>
-        로딩
+      <BootstrapButton variant="success" onClick={handleClick1}>
+        파일 로딩
       </BootstrapButton>
-      <AntdButton type="primary" onClick={handleClick}>
-        로딩
+      <AntdButton type="primary" onClick={handleClick2}>
+        서버 로딩
+      </AntdButton>
+
+      <AntdButton type="dashed" onClick={handleClick3}>
+        클리어
       </AntdButton>
       <ul className="songList">
         {songList.map((song) => {
