6bd0eda3c96901387b34bee42dc9f26f477e631d
JunSoo Lee <ljsay1113@naver.com>
2021-12-26 19:31:57 +0900

    스타일 개선

--- myreact30/src/components/Ball.jsx
+++ myreact30/src/components/Ball.jsx
@@ -2,14 +2,10 @@ import React, { memo } from 'react';
 
 const Ball = memo(({ number }) => {
   let background;
-  if (number < 10) {
-    background = 'red';
-  } else if (number < 20) {
+  if (number < 20) {
     background = 'orange';
-  } else if (number < 30) {
-    background = 'yellow';
   } else if (number < 40) {
-    background = 'blue';
+    background = 'yellow';
   } else {
     background = 'green';
   }
