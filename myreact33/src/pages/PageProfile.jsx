0a9d3659b12c4dce54eec887ef11cc062fef5775
JunSoo Lee <ljsay1113@naver.com>
2021-12-29 15:22:27 +0900

    json 데이터 가져오기, 클리어 버튼

--- myreact33/src/pages/PageProfile.jsx
+++ myreact33/src/pages/PageProfile.jsx
@@ -1,21 +1,37 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import Axios from 'axios';
 
 function PageProfile() {
-  const [profileList] = useState([
-    // {
-    //   uniqueId: 'bts-jin',
-    //   name: '진',
-    //   role: '서브보컬',
-    //   mbti: 'INFP',
-    //   instagramUrl: 'https://instagram.com/jin',
-    //   profileImageUrl:
-    //     'https://classdevopscloud.blob.core.windows.net/data/bts-jin.jpg',
-    // },
-  ]);
+  const handleRefresh = () => {
+    const [profileList, setProfileList] = useState([]);
+    useEffect(() => {
+      Axios.get(
+        'https://classdevopscloud.blob.core.windows.net/data/profile-list.json',
+      )
+        .then((response) => {
+          const newProfileList = response.data.map((profile) => ({
+            ...profile,
+            instagramUrl: profile.instagram_url,
+            profileImageUrl: profile.profile_image_url,
+          }));
+
+          setProfileList(newProfileList);
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    }, []);
+  };
+
+  const handleClear = () => {
+    return setProfileList([]);
+  };
 
   return (
     <div>
       <h2>PageProfile</h2>
+      <button onClick={handleClear}> 클리어 </button>
+      <button onClick={handleRefresh}> 새로고침 </button>
       {profileList.length === 0 && <h3>등록 된 프로필이 없습니다.</h3>}
       {profileList.map((member) => {
         return (
