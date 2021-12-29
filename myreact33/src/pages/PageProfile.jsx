b6e321b2556ff6cbbaf9e00ff7c10eba36720bfa
JunSoo Lee <ljsay1113@naver.com>
2021-12-29 18:56:19 +0900

    주소창 에러 시 에러 메세지 출력

--- myreact33/src/pages/PageProfile.jsx
+++ myreact33/src/pages/PageProfile.jsx
@@ -4,7 +4,10 @@ import Axios from 'axios';
 function PageProfile() {
   const [profileList, setProfileList] = useState([]);
 
+  const [error, setError] = useState(null);
+
   const handleRefresh = () => {
+    setError(null);
     Axios.get(
       'https://classdevopscloud.blob.core.windows.net/data/profile-list.json',
     )
@@ -18,7 +21,7 @@ function PageProfile() {
         setProfileList(newProfileList);
       })
       .catch((error) => {
-        console.error(error);
+        setError(error);
       });
   };
 
@@ -36,6 +39,7 @@ function PageProfile() {
       <button onClick={handleClear}> 클리어 </button>
       <button onClick={handleRefresh}> 새로고침 </button>
       {profileList.length === 0 && <h3>등록 된 프로필이 없습니다.</h3>}
+      {error && <h5>조회 시 에러가 발생했습니다.</h5>}
       {profileList.map((member) => {
         return (
           <div>
