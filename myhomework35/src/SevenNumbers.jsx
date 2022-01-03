a215ef0e0466bcc4ffbde2d33df603f51b91e02c
JunSoo Lee <ljsay1113@naver.com>
2022-01-03 18:23:20 +0900

    상탯값 초기화 및 랜덤 함수 구현

--- myhomework35/src/SevenNumbers.jsx
+++ myhomework35/src/SevenNumbers.jsx
@@ -1,4 +1,33 @@
+import { useReducer } from 'react';
+
+const makeNumbers = () => {
+  const randomNumber = [];
+  while (randomNumber.length < 7) {
+    const number = parseInt(Math.random() * 45) + 1;
+    if (randomNumber.indexOf(number) < 0) {
+      randomNumber.push(number);
+    }
+  }
+  randomNumber.sort((a, b) => {
+    return a - b;
+  });
+  return randomNumber;
+};
+
 function SevenNumbers() {
+  const [state, dispatch] = useReducer(reducer, {
+    numbers: [0, 0, 0, 0, 0, 0, 0],
+    colors: [
+      '#1B62BF',
+      '#1755A6',
+      '#37A647',
+      '#F29F05',
+      '#F23838',
+      'purple',
+      'pink',
+    ],
+  });
+
   return (
     <div>
       <h2>7개의 숫자</h2>
