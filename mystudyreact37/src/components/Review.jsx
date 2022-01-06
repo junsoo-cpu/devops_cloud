d7a9e30400b0e5dd1310e09fdb329c44e8aa0b43
JunSoo Lee <ljsay1113@naver.com>
2022-01-06 13:34:24 +0900

    리뷰 수정 삭제 추가

--- mystudyreact37/src/components/Review.jsx
+++ mystudyreact37/src/components/Review.jsx
@@ -1,10 +1,26 @@
 import './Review.css';
 import Star from './Star';
+import { useState } from 'react';
+
 function Review({ review }) {
+  const [showMenu, setShowMenu] = useState(false);
   return (
     <div
-      className={`m-1 p-1 rounded-lg text-lg border-green-500 border-2 hover:scale-105 cursor-pointer `}
+      onMouseEnter={() => setShowMenu(true)}
+      onMouseLeave={() => setShowMenu(false)}
+      className={`m-1 p-1 pt-3 rounded-lg  border-green-500 border-2 hover:scale-105 relative`}
     >
+      {showMenu && (
+        <div className="text-xs absolute top-0 right-0">
+          <span className="text-blue-700 hover:text-blue-300 cursor-pointer mr-2">
+            수정
+          </span>
+          <span className="text-red-700 hover:text-red-300 cursor-pointer">
+            삭제
+          </span>
+        </div>
+      )}
+
       {review.content}
       <Star score={review.number} />
     </div>
