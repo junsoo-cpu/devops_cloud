e50152cc45fed25bf98680b5d907a58615377fc4
JunSoo Lee <ljsay1113@naver.com>
2022-01-06 11:02:25 +0900

    별점 구현

--- mystudyreact37/src/components/Review.jsx
+++ mystudyreact37/src/components/Review.jsx
@@ -6,7 +6,7 @@ function Review({ review }) {
       className={`m-1 p-1 rounded-lg text-lg border-green-500 border-2 hover:scale-105 cursor-pointer `}
     >
       {review.content}
-      <Star scroe={review.number} />
+      <Star score={review.number} />
     </div>
   );
 }
